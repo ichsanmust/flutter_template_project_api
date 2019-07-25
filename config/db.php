e5e3117c92aff7ef23a11d74bc2c2fb1d17983e2
ichsanmust <ichsan.must10@gmail.com>
2019-07-25 09:57:42 +0700

    config di mac

--- config/db.php
+++ config/db.php
@@ -1,10 +1,16 @@
 <?php
 
 return [
+    // 'class' => 'yii\db\Connection',
+    // 'dsn' => 'mysql:host=localhost;dbname=flutter_template_project',
+    // 'username' => 'root',
+    // 'password' => '',
+    // 'charset' => 'utf8',
+
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=flutter_template_project',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=flutter_template_project',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root12345',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
